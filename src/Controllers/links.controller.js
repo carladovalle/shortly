98c60badb8332e15bb3e6b88054597ec6970d836
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-10-15 20:18:49 -0300

    feat: allows viewing url by id

--- src/Controllers/links.controller.js
+++ src/Controllers/links.controller.js
@@ -24,4 +24,30 @@ async function urlsShorten (req, res) {
 
 }
 
-export { urlsShorten };
+async function getUrl (req, res) {
+
+    const { id } = req.params;
+
+    if (id) {
+
+        try {
+
+            const { rows: urls } = await connection.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+ 
+            if (urls.length === 0) {
+                return res.sendStatus(404);
+            }
+
+            return res.status(200).send({id: urls[0].id, shortUrl: urls[0].shortUrl, url: urls[0].url});
+
+        } catch (error) {
+            return res.send(error.message);
+        }
+
+    } else {
+        return res.status(404).status('ID nulo');
+    }
+
+}
+
+export { urlsShorten, getUrl };
--- src/Routes/linksRouter.js
+++ src/Routes/linksRouter.js
@@ -6,5 +6,6 @@ import { validateLinks } from '../Middlewares/linksValidator.js';
 const router = express.Router();
 
 router.post('/urls/shorten', authToken, validateLinks, linksControllers.urlsShorten);
+router.get('/urls/:id', linksControllers.getUrl);
 
 export default router;
