66066977fb1e42de17ae330b891c3381e3ed5a97
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-10-15 08:41:46 -0300

    feat: adds encryption to the user's password when he registers

--- src/Controllers/signup.controller.js
+++ src/Controllers/signup.controller.js
@@ -1,14 +1,16 @@
 import connection from "../Database/database.js";
 import { signupSchema } from "../Schemas/signupSchema.js";
-
+import bcrypt from 'bcrypt';
 async function register (req, res) {
 
     const { name, email, password, confirmPassword } = req.body;
 
+    const hashPassword = bcrypt.hashSync(password, 10);
+
     try {
 
         await connection.query(
-            'INSERT INTO users (name, email, password, "confirmPassword") VALUES ($1, $2, $3, $4);', [name, email, password, confirmPassword]
+            'INSERT INTO users (name, email, password, "confirmPassword") VALUES ($1, $2, $3, $4);', [name, email, hashPassword, hashPassword]
         );
 
         return res.sendStatus(201);
